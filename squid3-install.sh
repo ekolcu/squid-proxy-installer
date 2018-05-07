a57232607c3cfd0d25ec88361f16f6ec69e70c7a
oh235 <39051269+oh235@users.noreply.github.com>
2018-05-08 01:42:52 +0800

    Update squid3-install.sh

--- squid3-install.sh
+++ squid3-install.sh
@@ -11,8 +11,8 @@ if cat /etc/os-release | grep PRETTY_NAME | grep "Ubuntu 16.04"; then
     touch /etc/squid/passwd
     /bin/rm -f /etc/squid/squid.conf
     /usr/bin/touch /etc/squid/blacklist.acl
-    /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/serverok/squid-proxy-installer/master/squid.conf
-    /sbin/iptables -I INPUT -p tcp --dport 3128 -j ACCEPT
+    /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/oh235/squid-proxy-installer/master/squid.conf
+    /sbin/iptables -I INPUT -p tcp --dport 2552 -j ACCEPT
     /sbin/iptables-save
     service squid restart
     update-rc.d squid defaults
@@ -22,8 +22,8 @@ elif cat /etc/*release | grep DISTRIB_DESCRIPTION | grep "Ubuntu 14.04"; then
     touch /etc/squid3/passwd
     /bin/rm -f /etc/squid3/squid.conf
     /usr/bin/touch /etc/squid3/blacklist.acl
-    /usr/bin/wget --no-check-certificate -O /etc/squid3/squid.conf https://raw.githubusercontent.com/serverok/squid-proxy-installer/master/squid.conf
-    /sbin/iptables -I INPUT -p tcp --dport 3128 -j ACCEPT
+    /usr/bin/wget --no-check-certificate -O /etc/squid3/squid.conf https://raw.githubusercontent.com/oh235/squid-proxy-installer/master/squid.conf
+    /sbin/iptables -I INPUT -p tcp --dport 2552 -j ACCEPT
     /sbin/iptables-save
     service squid3 restart
     ln -s /etc/squid3 /etc/squid
@@ -37,8 +37,8 @@ elif cat /etc/os-release | grep PRETTY_NAME | grep "jessie"; then
     touch /etc/squid3/passwd
     /bin/rm -f /etc/squid3/squid.conf
     /usr/bin/touch /etc/squid3/blacklist.acl
-    /usr/bin/wget --no-check-certificate -O /etc/squid3/squid.conf https://raw.githubusercontent.com/serverok/squid-proxy-installer/master/squid.conf
-    /sbin/iptables -I INPUT -p tcp --dport 3128 -j ACCEPT
+    /usr/bin/wget --no-check-certificate -O /etc/squid3/squid.conf https://raw.githubusercontent.com/oh235/squid-proxy-installer/master/squid.conf
+    /sbin/iptables -I INPUT -p tcp --dport 2552 -j ACCEPT
     /sbin/iptables-save
     service squid3 restart
     update-rc.d squid3 defaults
@@ -51,8 +51,8 @@ elif cat /etc/os-release | grep PRETTY_NAME | grep "stretch"; then
     touch /etc/squid/passwd
     /bin/rm -f /etc/squid/squid.conf
     /usr/bin/touch /etc/squid/blacklist.acl
-    /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/serverok/squid-proxy-installer/master/squid.conf
-    /sbin/iptables -I INPUT -p tcp --dport 3128 -j ACCEPT
+    /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/oh235/squid-proxy-installer/master/squid.conf
+    /sbin/iptables -I INPUT -p tcp --dport 2552 -j ACCEPT
     /sbin/iptables-save
     systemctl enable squid
     systemctl restart squid
